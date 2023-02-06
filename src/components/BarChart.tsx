b6fe0a6646604a26b5a2de6bc3c82ce5ed60bdb0
Zach Barbre <zach.barbre@gmail.com>
2023-02-06 15:58:43 -0500

    add axix and grid lines

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -1,6 +1,9 @@
+import { useMemo } from "react";
 import { Group } from "@visx/group";
 import { scaleLinear, scaleBand } from "@visx/scale";
 import { Bar } from "@visx/shape";
+import { AxisBottom, AxisLeft } from "@visx/axis";
+import { GridRows } from "@visx/grid";
 
 interface BarData {
   player: string;
@@ -10,35 +13,42 @@ interface BarProps {
   data: BarData[];
 }
 
-const margin = { top: 20, bottom: 20, left: 20, right: 20 };
+const margin = { top: 20, bottom: 40, left: 20, right: 10 };
 
 export function BarChart({ data }: BarProps) {
-  console.log("🚀 ~ file: BarChart.tsx:16 ~ BarChart ~ data", data);
-  const width = 500;
+  const width = 530;
   const height = 500;
 
-  const xMax = width;
+  const xMax = width - margin.left - margin.right;
   const yMax = height - margin.top - margin.bottom;
 
   const getPlayer = (d: BarData) => d.player;
   const getWins = (d: BarData) => d.wins;
 
-  console.log("🚀 ~ file: BarChart.tsx:27 ~ BarChart ~ scaleBand", scaleBand);
-  const xScale = scaleBand({
-    range: [0, xMax],
-    round: true,
-    domain: data.map(getPlayer),
-    padding: 0.5,
-  });
+  const xScale = useMemo(
+    () =>
+      scaleBand({
+        range: [0, xMax],
+        domain: data.map(getPlayer),
+        padding: 0.25,
+      }),
+    [xMax]
+  );
 
-  const yScale = scaleLinear({
-    range: [yMax, 0],
-    domain: [0, Math.max(...data.map(getWins))],
-  });
+  const yScale = useMemo(
+    () =>
+      scaleLinear({
+        range: [yMax, 0],
+        domain: [0, Math.max(...data.map(getWins))],
+      }),
+    [yMax]
+  );
 
   return (
     <svg width={width} height={height}>
-      <Group top={margin.top}>
+      <rect fill="#04002b" width={width} height={height} rx={10} />
+      <Group left={margin.left} top={margin.top}>
+        <GridRows scale={yScale} width={xMax} height={yMax} />
         {data.map((d) => {
           const barHeight = yMax - yScale(getWins(d));
           return (
@@ -52,6 +62,29 @@ export function BarChart({ data }: BarProps) {
             />
           );
         })}
+        <AxisBottom
+          top={yMax}
+          scale={xScale}
+          stroke="white"
+          tickStroke="white"
+          tickLabelProps={() => ({
+            fill: "white",
+            fontSize: 11,
+            textAnchor: "middle",
+          })}
+        />
+        <AxisLeft
+          scale={yScale}
+          stroke="white"
+          tickStroke="white"
+          tickLabelProps={() => ({
+            fill: "white",
+            fontSize: 11,
+            textAnchor: "end",
+            dx: "-0.25em",
+            dy: "0.25em",
+          })}
+        />
       </Group>
     </svg>
   );
