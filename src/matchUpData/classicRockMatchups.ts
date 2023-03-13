523bca38d1a43fdb68ddbc02fbd6679d2af37ae2
Zach Barbre <zach.barbre@gmail.com>
2023-03-13 15:19:26 -0400

    update first t rex song link

--- src/matchUpData/classicRockMatchups.ts
+++ src/matchUpData/classicRockMatchups.ts
@@ -4,10 +4,10 @@ export default [
     matchup: 1,
     winner: "T. Rex",
     winningSong:
-      "https://open.spotify.com/track/0xKsqM6m90xy3jKnfpQh4I?si=Skq2BgtQQjqHMZEoL8b0pg&utm_source=copy-link",
+      "https://open.spotify.com/track/66hdYurKbEfP74fvWtR7mj?si=Gm9BiffvQXiU-EVF8dGRJA",
     loser: "Harry Nilsson",
     losingSong:
-      "https://open.spotify.com/track/6MrILERJLA3Jcq0E9poZsp?si=uZ4iLDSVTGalp2kZvyD2BA&nd=1",
+      "https://open.spotify.com/track/6MrILERJLA3Jcq0E9poZsp?si=uZ4iLDSVTGalp2kZvyD2BA",
     winVotes: 9,
     lossVotes: 2,
     judgeVote: false,
