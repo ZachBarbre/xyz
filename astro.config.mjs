60af1b0f9ec538bc590db72b1dd73e7800c3ad1b
Zach Barbre <zach.barbre@gmail.com>
2024-02-08 10:43:21 -0500

    add animation toggle

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import react from "@astrojs/react";
-
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()]
+  integrations: [react(), mdx()],
 });
